ecefd359b104bafcfae534ea03afdb08a402c87d
Zach Ripka <zripka@gmail.com>
2020-03-11 18:22:45 -0400

    I haven't super tested everything yet but I think it works

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ A simple bash script to copy tagged text from interview transcripts to a new fil
 ### structure
 * relevant text in the transcript files is enclosed by tags
   * start tag: @@
-  * end tag ~~
+  * end tag: ~~
 * each interview is assigned a code
 * all text inside of an @@ ~~ pair is copied into a new text file and has the interview ID appended to the end of the line
 * command arguments: input filename, output filename, interview id
+
+### preparing transcripts
+* create a table corresponding interviewee names to interview ids
+* do a find-and-replace on all existing '@' or '~' symbols
--- tag-parse.sh
+++ tag-parse.sh
@@ -4,13 +4,11 @@
 if [ $# -eq 0 ]
 then
   echo "Command line arguments: input filename, output filename, interview id"
+  exit 0
 fi
 
-# format the text file with fmt to break up long lines of text originally without newline
-
-# search the text file for text enclosed by @@ and ~~ tags
-
-# write to new text file: line number + tagged text + (interview ID) + new line
+# search the text file for text enclosed by @@ and ~~ tags, add the interview id to each tagged section, strip @ and ~ characters, and write to new text file
+grep -Eo '@[^~]*~' $1 | sed -e "s/^/$3 /" | sed -E 's/@//' | sed -E 's/~//'>> $2
 
 # exit the script
 exit 0
