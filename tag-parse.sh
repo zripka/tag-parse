44d68ee709f1c93f6923f9d7bc20bf4fae118958
Zach Ripka <zripka@gmail.com>
2020-03-17 12:46:38 -0400

    staying up to date with small changes

--- tag-parse.sh
+++ tag-parse.sh
@@ -7,6 +7,19 @@ then
   exit 0
 fi
 
+read -p "Enter participant alias: " alias
+read -p "Enter today's date (YY-MM-DD): " date
+
+# append information into header
+echo "Undergraduate thesis
+
+'Relevant Text' stage of qualitative data analysis (Auerbach & Silverstein 2003)
+Zach Ripka
+Participant $alias
+date: $date
+
+" >> $2
+
 # search the text file for text enclosed by @@ and ~~ tags, add the interview id to each tagged section, strip @ and ~ characters, and write to new text file
 echo $(cat $1) | grep -Eo '@[^~]*~' | sed -e "s/^/$3 /" | sed '/$/ G' | sed -E 's/@//' | sed -E 's/~//' >> $2
 
